f1358a2090e60ab655bb6d6407c6a0951dc84c5f
Rob <lum.robert@gmail.com>
2013-12-11 11:58:38 -0500

    Reddit Search API JS, HTML, CSS v1

---
+++ Reddit Search API/about.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    console.log('doc ready');
+
+    $('#submit').click(function(){
+    	searchBox = $('#textBox').attr('value');
+        var searchTerm = {
+            q: searchBox,
+			jsonp: 'process'
+        };
+        search(searchTerm);
+    });
+});
+
+function search(searchTerm) {
+    console.log('searching for ');
+    console.dir(searchTerm);
+
+     $.ajax({
+        url:'http://www.reddit.com/r/subreddits/search.json?' + $.param(searchTerm),
+        dataType: 'jsonp'
+    });
+}
+
+function process(data){
+	$.each(
+      data.data.children,
+      function (i, item) {
+      	$("ul#redditPosts").addClass("borderClass")
+        $("#redditPosts").append('<h2>SubReddit: ' + item.data.subreddit + '</h2>');
+        $("#redditPosts").append('<p class="link">' + item.data.url + '</p>');
+        $("#redditPosts").append('<p>' + item.data.selftext + '</p>');
+        $("#redditPosts").append('<hr><br />');
+      }
+    )
+}
