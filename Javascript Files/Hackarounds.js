f59d0a8eb032e4d72129c0dfd210c427127d1aee
Rob <lum.robert@gmail.com>
2014-05-05 18:04:01 -0400

    old youtube resize script for convenience

--- Javascript Files/Hackarounds.js
+++ Javascript Files/Hackarounds.js
@@ -1,7 +1,18 @@
 Hackarounds
 
-Target nameless element on page with style
+//Target nameless element on page with style
 $("div [style*='#888']").hide();
 
-Target text node in nameless element
+//Target text node in nameless element
 $(".CFile").contents().filter(function(){ return this.nodeType == 3; }).remove();
+
+//Youtube window resize
+var windowWidth = $(window).width(),
+youtubeHeight = windowWidth * 0.75;
+$('#youtubeVideo').attr('height', youtubeHeight);
+
+$(window).resize(function(){
+	var windowWidth = $(window).width(),
+	youtubeHeight = windowWidth * 0.75;
+	$('#youtubeVideo').attr('height', youtubeHeight);
+});
