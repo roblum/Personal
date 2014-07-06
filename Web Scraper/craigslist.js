a19f87a8fa26033a22d6fd0b32ea3d21d348d468
Robert <lum.robert@gmail.com>
2014-07-05 22:03:19 -0400

    created craigslist scraper for bike wheel

---
+++ Web Scraper/craigslist.js
@@ -0,0 +1,40 @@
+var express 	= require('express')
+	,fs 		= require('fs')
+	,request 	= require('request')
+	,cheerio 	= require('cheerio')
+	,app     	= express();
+
+app.get('/scrape', function(req, res){
+	
+	url = 'http://newyork.craigslist.org/search/sss?query=aerospoke&sort=rel';
+
+	request(url, function(error, response, html){
+		if(!error){
+			console.log(html);
+			var $ = cheerio.load(html);
+
+			var price, itemName;
+			var json = { price : "", itemName : ""};
+
+			$('meta[name=description]').filter(function(){
+		        var data = $(this);
+		        console.log(data)
+		        itemName = data.attr('content');
+		        json.itemName = itemName;
+	        })
+
+		}
+
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+
+        	console.log('File successfully written!');
+
+        })
+
+        res.send('Scrape complete')
+	})
+})
+
+app.listen('8081')
+console.log('Running on port 8081');
+exports = module.exports = app;
